5f0d09b04e328de4d89c4e01c4eb3f441cba7d77
krille-chan <krille-chan@posteo.de>
2023-09-15 10:25:19 +0200

    feat: Dark mode for status page

--- src/status_handler.dart
+++ src/status_handler.dart
@@ -13,20 +13,20 @@ Future<Response> statusHandler(Request request, Config config) async {
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="container space-y-8 mx-auto h-screen w-screen flex flex-col justify-center items-center">
+<body class="bg-white dark:bg-slate-800 container space-y-8 mx-auto h-screen w-screen flex flex-col justify-center items-center">
     <h1 class="block text-5xl">🚀</h1>
-    <p><b>FluffyGate</b> is up and running.</p>
+    <p class="dark:text-white"><b>FluffyGate</b> is up and running.</p>
     <a href="${config.appWebsite}"
-        class="bg-transparent hover:bg-indigo-500 text-indigo-700 transition-colors font-semibold hover:text-white py-2 px-4 border border-indigo-500 hover:border-transparent rounded-xl">
+        class="bg-transparent hover:bg-indigo-500 text-indigo-700 dark:text-indigo-300 transition-colors font-semibold hover:text-white py-2 px-4 border border-indigo-500 dark:border-indigo-300 hover:border-transparent rounded-xl">
         Install ${config.appName}
     </a>
     <span>
-        <a class="text-indigo-500 hover:text-indigo-700 underline"
+        <a class="text-indigo-500 dark:text-indigo-300 hover:text-indigo-700 underline"
             href="https://github.com/krille-chan/fluffygate">Source
             Code</a> -
-        <a class="text-indigo-500 hover:text-indigo-700 underline"
+        <a class="text-indigo-500 dark:text-indigo-300 hover:text-indigo-700 underline"
             href="https://github.com/krille-chan/fluffygate/blob/main/LICENSE">License</a> -
-        <a class="text-indigo-500 hover:text-indigo-700 underline"
+        <a class="text-indigo-500 dark:text-indigo-300 hover:text-indigo-700 underline"
             href="https://matrix.org">About [matrix]</a>
     </span>
 </body>
