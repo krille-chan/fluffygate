3905c0b3b4eb0460228f02634ae4982adb2ea3d6
krille-chan <krille-chan@posteo.de>
2023-09-15 09:31:21 +0200

    fix: Mixed up app names

--- src/status_handler.dart
+++ src/status_handler.dart
@@ -15,10 +15,10 @@ Future<Response> statusHandler(Request request, Config config) async {
 
 <body class="container space-y-8 mx-auto h-screen w-screen flex flex-col justify-center items-center">
     <h1 class="block text-5xl">🚀</h1>
-    <p><b>${config.appName}</b> is up and running.</p>
+    <p><b>FluffyGate</b> is up and running.</p>
     <a href="${config.appWebsite}"
         class="bg-transparent hover:bg-indigo-500 text-indigo-700 transition-colors font-semibold hover:text-white py-2 px-4 border border-indigo-500 hover:border-transparent rounded-xl">
-        Install FluffyChat
+        Install ${config.appName}
     </a>
     <span>
         <a class="text-indigo-500 hover:text-indigo-700 underline"
