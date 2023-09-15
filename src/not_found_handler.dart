3c473ce049c5976ab51f1cebd3b86ff7328823bd
krille-chan <krille-chan@posteo.de>
2023-09-15 10:27:57 +0200

    fix: Not found returns status code 200

--- src/not_found_handler.dart
+++ src/not_found_handler.dart
@@ -1,6 +1,6 @@
 import 'package:shelf/shelf.dart';
 
-Future<Response> notFoundHandler(Request _) async => Response.ok(
+Future<Response> notFoundHandler(Request _) async => Response.notFound(
       '{"errcode":"M_UNRECOGNIZED","error":"Unrecognized request"}',
       headers: {'Content-Type': 'application/json'},
     );
